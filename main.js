9cf69d2510947b67acda057e98efaaa2f2b95711
Natasa90 <natasa.golubovic90@gmail.com>
2024-03-06 12:13:31 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let age = 34; // number 
+let bigNumber = 9007199254740991n; // bigint
+let firstName = "Natasa"; //string 
+var a = 10 == 10; // boolean 
+var b = null; // null 
+var c; // undefined 
+let car = {make:"Honda", model:"Civic", year:2001}; //object
