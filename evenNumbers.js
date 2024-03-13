59ca93efcf1312281ed69a8ab5bb3ae6d0d09272
Natasa90 <natasa.golubovic90@gmail.com>
2024-03-13 17:42:19 +0100

    Homework 03/13/2024

---
+++ evenNumbers.js
@@ -0,0 +1,12 @@
+let n = 10;
+let evenNumbers = 0; 
+for (let i = 1; i <= n; i++){
+    if (i % 2 === 0){
+        evenNumbers = evenNumbers + 1;
+}
+}
+console.log(evenNumbers);
+
+
+
+
---
+++ sumNumb2.js
@@ -0,0 +1,6 @@
+let num = 10;
+let sum = 0;
+for (let i = 0; i <= num; i++){
+    sum = sum + i; // we were stuck here - tried with sum = sum + num, and sum = sum + 1;
+}
+console.log(sum);
